3cf6826e84eb6e79c1f6c35eee2aa2c5f738464d
himaja-e <105623310+himaja-e@users.noreply.github.com>
2022-11-17 23:26:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const hello = () => {
 }
 
 hello();
+
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer((request, response) => {
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      response.write(home);
+      response.end();
+    })
+    .listen(5000);
+});
